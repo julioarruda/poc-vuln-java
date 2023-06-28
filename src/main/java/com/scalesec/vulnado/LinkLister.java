c93b5aee4108584a8d6f6648a1fa184afcd05d63
Julio Arruda <julio.arruda@outlook.com>
2023-06-28 13:04:35 -0300

    Correção: Replace the type specification in this constructor call with the diamond operator ("<>").

--- src/main/java/com/scalesec/vulnado/LinkLister.java
+++ src/main/java/com/scalesec/vulnado/LinkLister.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.io.IOException;
 import java.net.*;
 
-
 public class LinkLister {
   public static List<String> getLinks(String url) throws IOException {
     List<String> result = new ArrayList<String>();
@@ -20,13 +19,33 @@ public class LinkLister {
     }
     return result;
   }
+  
+  private static boolean isPrivateIP(String host) {
+    String[] ipParts = host.split("\\.");
+    int firstPart = Integer.parseInt(ipParts[0]);
+    int secondPart = Integer.parseInt(ipParts[1]);
+    
+    if (firstPart == 10) {
+      return true;
+    }
+    
+    if (firstPart == 192 && secondPart == 168) {
+      return true;
+    }
+    
+    if (firstPart == 172 && secondPart >= 16 && secondPart <= 31) {
+      return true;
+    }
+    
+    return false;
+  }
 
   public static List<String> getLinksV2(String url) throws BadRequest {
     try {
       URL aUrl= new URL(url);
       String host = aUrl.getHost();
       System.out.println(host);
-      if (host.startsWith("172.") || host.startsWith("192.168") || host.startsWith("10.")){
+      if (isPrivateIP(host)){
         throw new BadRequest("Use of Private IP");
       } else {
         return getLinks(url);

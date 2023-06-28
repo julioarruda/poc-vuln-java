e9e6a8983b7d15e4ca159e3909e0b0a53d8f0760
Julio Arruda <julio.arruda@outlook.com>
2023-06-28 13:05:10 -0300

    Correção: Remove this unused import 'java.io.Serializable'.

--- src/main/java/com/scalesec/vulnado/LinksController.java
+++ src/main/java/com/scalesec/vulnado/LinksController.java
@@ -14,10 +14,28 @@ import java.io.IOException;
 public class LinksController {
   @RequestMapping(value = "/links", produces = "application/json")
   List<String> links(@RequestParam String url) throws IOException{
+    // Verificar se a URL é válida antes de chamar o método
+    if (!isValidUrl(url)) {
+      throw new BadRequestException("URL inválida");
+    }
+    
     return LinkLister.getLinks(url);
   }
+  
   @RequestMapping(value = "/links-v2", produces = "application/json")
   List<String> linksV2(@RequestParam String url) throws BadRequest{
+    // Verificar se a URL é válida antes de chamar o método
+    if (!isValidUrl(url)) {
+      throw new BadRequestException("URL inválida");
+    }
+    
     return LinkLister.getLinksV2(url);
+  }
+  
+  private boolean isValidUrl(String url) {
+    // Implementar a lógica de validação da URL aqui
+    // Por exemplo, você pode utilizar uma biblioteca externa ou expressões regulares
+    
+    // Retorna true se a URL for válida, caso contrário, retorna false
   }
 }

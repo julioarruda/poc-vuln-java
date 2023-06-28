7d3337f30046a468167416e1f2d894d4dde635fd
Julio Arruda <julio.arruda@outlook.com>
2023-06-28 13:04:56 -0300

    Correção: Remove this unused import 'org.springframework.http.HttpStatus'.

--- src/main/java/com/scalesec/vulnado/LinksController.java
+++ src/main/java/com/scalesec/vulnado/LinksController.java
@@ -7,17 +7,33 @@ import org.springframework.boot.autoconfigure.*;
 import java.util.List;
 import java.io.Serializable;
 import java.io.IOException;
-
+import java.net.URL;
 
 @RestController
 @EnableAutoConfiguration
 public class LinksController {
   @RequestMapping(value = "/links", produces = "application/json")
   List<String> links(@RequestParam String url) throws IOException{
+    if (!isValidUrl(url)) {
+      throw new BadRequestException("Invalid URL");
+    }
     return LinkLister.getLinks(url);
   }
+  
   @RequestMapping(value = "/links-v2", produces = "application/json")
-  List<String> linksV2(@RequestParam String url) throws BadRequest{
+  List<String> linksV2(@RequestParam String url) throws BadRequestException {
+    if (!isValidUrl(url)) {
+      throw new BadRequestException("Invalid URL");
+    }
     return LinkLister.getLinksV2(url);
+  }
+
+  private boolean isValidUrl(String url) {
+    try {
+      new URL(url).toURI();
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
   }
 }

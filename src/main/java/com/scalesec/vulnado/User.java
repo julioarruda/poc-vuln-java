bc97de3d1b8c7811923bd55fcbee11d017a876bc
Julio Arruda <julio.arruda@outlook.com>
2023-06-29 10:45:40 -0300

    Correção: Change this code to not construct SQL queries directly from user-controlled data.

--- src/main/java/com/scalesec/vulnado/User.java
+++ src/main/java/com/scalesec/vulnado/User.java
@@ -1,7 +1,9 @@
+
+
 package com.scalesec.vulnado;
 
 import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.JwtParser;
@@ -37,16 +39,17 @@ public class User {
   }
 
   public static User fetch(String un) {
-    Statement stmt = null;
+    PreparedStatement pstmt = null;
     User user = null;
     try {
       Connection cxn = Postgres.connection();
-      stmt = cxn.createStatement();
       System.out.println("Opened database successfully");
 
-      String query = "select * from users where username = '" + un + "' limit 1";
+      String query = "SELECT * FROM users WHERE username = ? LIMIT 1";
+      pstmt = cxn.prepareStatement(query);
+      pstmt.setString(1, un);
       System.out.println(query);
-      ResultSet rs = stmt.executeQuery(query);
+      ResultSet rs = pstmt.executeQuery();
       if (rs.next()) {
         String user_id = rs.getString("user_id");
         String username = rs.getString("username");

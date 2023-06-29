a5fee90de811b06e66257997ee7442772073119a
Julio Arruda <julio.arruda@outlook.com>
2023-06-28 21:12:25 -0300

    Correção: Change this code to not construct SQL queries directly from user-controlled data.

--- src/main/java/com/scalesec/vulnado/LoginController.java
+++ src/main/java/com/scalesec/vulnado/LoginController.java
@@ -14,11 +14,11 @@ public class LoginController {
   @Value("${app.secret}")
   private String secret;
 
-  @CrossOrigin(origins = "*")
+  @CrossOrigin(origins = "https://trusted-website.com")
   @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
   LoginResponse login(@RequestBody LoginRequest input) {
     User user = User.fetch(input.username);
-    if (Postgres.md5(input.password).equals(user.hashedPassword)) {
+    if (user != null && Postgres.md5(input.password).equals(user.hashedPassword)) {
       return new LoginResponse(user.token(secret));
     } else {
       throw new Unauthorized("Access Denied");
@@ -27,17 +27,21 @@ public class LoginController {
 }
 
 class LoginRequest implements Serializable {
+  private static final long serialVersionUID = 1L;
   public String username;
   public String password;
 }
 
 class LoginResponse implements Serializable {
+  private static final long serialVersionUID = 1L;
   public String token;
   public LoginResponse(String msg) { this.token = msg; }
 }
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
 class Unauthorized extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+
   public Unauthorized(String exception) {
     super(exception);
   }

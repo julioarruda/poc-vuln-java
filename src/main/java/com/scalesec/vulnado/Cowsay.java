1a6cc9ce94f44c83035bc761f2745c8fec68b1f8
Julio Arruda <julio.arruda@outlook.com>
2023-06-30 15:18:24 -0300

    Correção: Make sure the "PATH" used to find this command includes only what you intend.

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -1,14 +1,23 @@
+
+
 package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Cowsay {
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
-    processBuilder.command("bash", "-c", cmd);
+    List<String> commands = new ArrayList<>(Arrays.asList("/usr/games/cowsay"));
+    if (input.matches("[A-Za-z0-9\\s]+")) {
+      commands.addAll(Arrays.asList(input.split(" ")));
+      processBuilder.command(commands);
+    } else {
+      throw new IllegalArgumentException("Invalid input.");
+    }
 
     StringBuilder output = new StringBuilder();
 

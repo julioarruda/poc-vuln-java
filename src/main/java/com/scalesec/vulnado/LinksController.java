d268b690ef172639fd6f1a6811b31dd8a28acfbd
Julio Arruda <julio.arruda@outlook.com>
2023-07-03 13:52:25 -0300

    Correção: Make sure allowing safe and unsafe HTTP methods is safe here.

--- src/main/java/com/scalesec/vulnado/LinksController.java
+++ src/main/java/com/scalesec/vulnado/LinksController.java
@@ -1,3 +1,5 @@
+
+
 package com.scalesec.vulnado;
 
 import org.springframework.boot.*;
@@ -8,16 +10,35 @@ import java.util.List;
 import java.io.Serializable;
 import java.io.IOException;
 
-
 @RestController
 @EnableAutoConfiguration
 public class LinksController {
   @RequestMapping(value = "/links", produces = "application/json")
-  List<String> links(@RequestParam String url) throws IOException{
+  List<String> links(@RequestParam String url) throws IOException {
+    if (!isTrustedURL(url)) {
+      throw new BadRequest("URL não confiável.");
+    }
     return LinkLister.getLinks(url);
   }
+
   @RequestMapping(value = "/links-v2", produces = "application/json")
-  List<String> linksV2(@RequestParam String url) throws BadRequest{
+  List<String> linksV2(@RequestParam String url) throws BadRequest {
+    if (!isTrustedURL(url)) {
+      throw new BadRequest("URL não confiável.");
+    }
     return LinkLister.getLinksV2(url);
+  }
+
+  // Método para verificar se a URL é confiável
+  private boolean isTrustedURL(String url) {
+    // Lista de URLs confiáveis
+    String[] trustedURLs = {"https://gft.com/br"};
+
+    for (String trustedURL : trustedURLs) {
+      if (url.equals(trustedURL)) {
+        return true;
+      }
+    }
+    return false;
   }
 }

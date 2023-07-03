c40d33c30da8efecaca0967f67765719d44c2cab
Julio Arruda <julio.arruda@outlook.com>
2023-07-03 11:15:52 -0300

    Correção: Make sure that enabling CORS is safe here.

--- src/main/java/com/scalesec/vulnado/LoginController.java
+++ src/main/java/com/scalesec/vulnado/LoginController.java
@@ -1,3 +1,5 @@
+-
+
 package com.scalesec.vulnado;
 
 import org.springframework.boot.*;
@@ -14,7 +16,7 @@ public class LoginController {
   @Value("${app.secret}")
   private String secret;
 
-  @CrossOrigin(origins = "*")
+  @CrossOrigin(origins = "https://gft.com/br")
   @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
   LoginResponse login(@RequestBody LoginRequest input) {
     User user = User.fetch(input.username);

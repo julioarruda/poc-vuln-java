11e6508d2dec0e61fbd0d606374a616409778f2c
Julio Arruda <julio.arruda@outlook.com>
2023-07-03 13:29:00 -0300

    Correção: Make sure that this user-controlled command argument doesn't lead to unwanted behavior.

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -1,3 +1,5 @@
+
+
 package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
@@ -6,7 +8,10 @@ import java.io.InputStreamReader;
 public class Cowsay {
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
+    // Adicionar a linha abaixo para criar um input sanitizado
+    String sanitizedInput = input.replaceAll("[^a-zA-Z0-9 ]", "");
+    // Modificar a linha abaixo, substituindo 'input' por 'sanitizedInput'
+    String cmd = "/usr/games/cowsay '" + sanitizedInput + "'";
     System.out.println(cmd);
     processBuilder.command("bash", "-c", cmd);
 

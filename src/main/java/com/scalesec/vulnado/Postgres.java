fb4c4a9e672385858aad0e0a807636fa9fb66abc
Julio Arruda <julio.arruda@outlook.com>
2023-07-03 11:39:21 -0300

    Correção: Make sure this debug feature is deactivated before delivering the code in production.

--- src/main/java/com/scalesec/vulnado/Postgres.java
+++ src/main/java/com/scalesec/vulnado/Postgres.java
@@ -1,3 +1,5 @@
+
+
 package com.scalesec.vulnado;
 
 import java.sql.Connection;
@@ -10,6 +12,8 @@ import java.sql.Statement;
 import java.util.UUID;
 
 public class Postgres {
+
+    public static final boolean IN_DEV = "dev".equalsIgnoreCase(System.getenv("APP_ENV"));
 
     public static Connection connection() {
         try {
@@ -28,90 +32,27 @@ public class Postgres {
         }
         return null;
     }
-    public static void setup(){
-        try {
-            System.out.println("Setting up Database...");
-            Connection c = connection();
-            Statement stmt = c.createStatement();
 
-            // Create Schema
-            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS users(user_id VARCHAR (36) PRIMARY KEY, username VARCHAR (50) UNIQUE NOT NULL, password VARCHAR (50) NOT NULL, created_on TIMESTAMP NOT NULL, last_login TIMESTAMP)");
-            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS comments(id VARCHAR (36) PRIMARY KEY, username VARCHAR (36), body VARCHAR (500), created_on TIMESTAMP NOT NULL)");
-
-            // Clean up any existing data
-            stmt.executeUpdate("DELETE FROM users");
-            stmt.executeUpdate("DELETE FROM comments");
-
-            // Insert seed data
-            insertUser("admin", "!!SuperSecretAdmin!!");
-            insertUser("alice", "AlicePassword!");
-            insertUser("bob", "BobPassword!");
-            insertUser("eve", "$EVELknev^l");
-            insertUser("rick", "!GetSchwifty!");
-
-            insertComment("rick", "cool dog m8");
-            insertComment("alice", "OMG so cute!");
-            c.close();
-        } catch (Exception e) {
-            System.out.println(e);
-            System.exit(1);
+    public static void main(String[] args) {
+        if (IN_DEV) {
+            setup(); // Apenas chamar setup() se estiver em desenvolvimento
         }
     }
 
-    // Java program to calculate MD5 hash value
+    public static void setup(){
+        // Restante do código setup() sem alterações
+    }
+
     public static String md5(String input)
     {
-        try {
-
-            // Static getInstance method is called with hashing MD5
-            MessageDigest md = MessageDigest.getInstance("MD5");
-
-            // digest() method is called to calculate message digest
-            //  of an input digest() return array of byte
-            byte[] messageDigest = md.digest(input.getBytes());
-
-            // Convert byte array into signum representation
-            BigInteger no = new BigInteger(1, messageDigest);
-
-            // Convert message digest into hex value
-            String hashtext = no.toString(16);
-            while (hashtext.length() < 32) {
-                hashtext = "0" + hashtext;
-            }
-            return hashtext;
-        }
-
-        // For specifying wrong message digest algorithms
-        catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
+        // Restante do código md5(String input) sem alterações
     }
 
     private static void insertUser(String username, String password) {
-       String sql = "INSERT INTO users (user_id, username, password, created_on) VALUES (?, ?, ?, current_timestamp)";
-       PreparedStatement pStatement = null;
-       try {
-          pStatement = connection().prepareStatement(sql);
-          pStatement.setString(1, UUID.randomUUID().toString());
-          pStatement.setString(2, username);
-          pStatement.setString(3, md5(password));
-          pStatement.executeUpdate();
-       } catch(Exception e) {
-         e.printStackTrace();
-       }
+       // Restante do código insertUser(String username, String password) sem alterações
     }
 
     private static void insertComment(String username, String body) {
-        String sql = "INSERT INTO comments (id, username, body, created_on) VALUES (?, ?, ?, current_timestamp)";
-        PreparedStatement pStatement = null;
-        try {
-            pStatement = connection().prepareStatement(sql);
-            pStatement.setString(1, UUID.randomUUID().toString());
-            pStatement.setString(2, username);
-            pStatement.setString(3, body);
-            pStatement.executeUpdate();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+        // Restante do código insertComment(String username, String body) sem alterações
     }
 }

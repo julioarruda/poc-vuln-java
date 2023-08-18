3d0ca5352b415f7ffa3b6954af509e4127f4b5fc
Julio Arruda <julio.arruda@outlook.com>
2023-08-18 10:32:57 -0300

    Correção: Make sure the "PATH" used to find this command includes only what you intend.

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -1,3 +1,5 @@
+
+
 package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
@@ -6,9 +8,8 @@ import java.io.InputStreamReader;
 public class Cowsay {
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
-    processBuilder.command("bash", "-c", cmd);
+    processBuilder.command("/usr/games/cowsay", input);
+    System.out.println("/usr/games/cowsay " + input);
 
     StringBuilder output = new StringBuilder();
 

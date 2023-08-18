c46043a8832996984835fa5278905f9147327230
Julio Arruda <julio.arruda@outlook.com>
2023-08-18 10:08:40 -0300

    Correção: Make sure this debug feature is deactivated before delivering the code in production.

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -1,3 +1,5 @@
+
+
 package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
@@ -7,7 +9,11 @@ public class Cowsay {
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
     String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
+    
+    if (System.getenv("DEBUG") != null && System.getenv("DEBUG").equalsIgnoreCase("true")) {
+        System.out.println(cmd);
+    }
+
     processBuilder.command("bash", "-c", cmd);
 
     StringBuilder output = new StringBuilder();

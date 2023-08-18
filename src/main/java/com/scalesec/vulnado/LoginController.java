209a119fb7a3904b314394bee2491fd3e6a154e0
Julio Arruda <julio.arruda@outlook.com>
2023-08-18 07:39:52 -0300

    Correção: Change this code to not construct SQL queries directly from user-controlled data.

--- src/main/java/com/scalesec/vulnado/LoginController.java
+++ src/main/java/com/scalesec/vulnado/LoginController.java
@@ -1,3 +1,5 @@
+
+
 package com.scalesec.vulnado;
 
 import org.springframework.boot.*;
@@ -6,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import java.io.Serializable;
 
 @RestController
@@ -14,11 +18,12 @@ public class LoginController {
   @Value("${app.secret}")
   private String secret;
 
-  @CrossOrigin(origins = "*")
+  @CrossOrigin(origins = "https://gft.com")
   @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
   LoginResponse login(@RequestBody LoginRequest input) {
     User user = User.fetch(input.username);
-    if (Postgres.md5(input.password).equals(user.hashedPassword)) {
+    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    if (passwordEncoder.matches(input.password, user.hashedPassword)) {
       return new LoginResponse(user.token(secret));
     } else {
       throw new Unauthorized("Access Denied");

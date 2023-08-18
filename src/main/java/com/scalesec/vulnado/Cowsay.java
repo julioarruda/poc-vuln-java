397621b86920f7a643202b8c73d79cf59fc61340
Julio Arruda <julio.arruda@outlook.com>
2023-08-18 09:19:29 -0300

    Correção: Make sure that this user-controlled command argument doesn't lead to unwanted behavior.

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -1,11 +1,23 @@
+
+
 package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Cowsay {
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
+    
+    // Adicione a verificação se o input contém apenas caracteres permitidos
+    Pattern pattern = Pattern.compile("^[a-zA-Z0-9\\s]*$");
+    Matcher matcher = pattern.matcher(input);
+    if (!matcher.matches()) {
+      throw new IllegalArgumentException("Caracteres inválidos no input");
+    }
+
     String cmd = "/usr/games/cowsay '" + input + "'";
     System.out.println(cmd);
     processBuilder.command("bash", "-c", cmd);

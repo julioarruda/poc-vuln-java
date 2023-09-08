63240fa57c36a7c9eb00fbc3953d20e9920d1ffe
Julio Arruda <julio.arruda@outlook.com>
2023-09-08 13:29:45 -0300

    Correção: Make sure allowing safe and unsafe HTTP methods is safe here.

--- src/main/java/com/scalesec/vulnado/LinksController.java
+++ src/main/java/com/scalesec/vulnado/LinksController.java
@@ -1,3 +1,6 @@
+-------------------------
+
+
 package com.scalesec.vulnado;
 
 import org.springframework.boot.*;
@@ -8,16 +11,18 @@ import java.util.List;
 import java.io.Serializable;
 import java.io.IOException;
 
-
 @RestController
 @EnableAutoConfiguration
 public class LinksController {
-  @RequestMapping(value = "/links", produces = "application/json")
+  
+  @GetMapping(value = "/links", produces = "application/json")
   List<String> links(@RequestParam String url) throws IOException{
     return LinkLister.getLinks(url);
   }
-  @RequestMapping(value = "/links-v2", produces = "application/json")
+
+  @GetMapping(value = "/links-v2", produces = "application/json")
   List<String> linksV2(@RequestParam String url) throws BadRequest{
     return LinkLister.getLinksV2(url);
   }
+  
 }

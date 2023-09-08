93eaa5781d5bfea0e9de5db0a97f11423f18b534
Julio Arruda <julio.arruda@outlook.com>
2023-09-08 11:45:21 -0300

    Correção: Make sure this debug feature is deactivated before delivering the code in production.

--- src/main/java/com/scalesec/vulnado/User.java
+++ src/main/java/com/scalesec/vulnado/User.java
@@ -1,3 +1,7 @@
+-----
+
+
+
 package com.scalesec.vulnado;
 
 import java.sql.Connection;
@@ -31,7 +35,8 @@ public class User {
         .setSigningKey(key)
         .parseClaimsJws(token);
     } catch(Exception e) {
-      e.printStackTrace();
+      //Debug resource
+      //e.printStackTrace();
       throw new Unauthorized(e.getMessage());
     }
   }
@@ -42,21 +47,29 @@ public class User {
     try {
       Connection cxn = Postgres.connection();
       stmt = cxn.createStatement();
-      System.out.println("Opened database successfully");
+      //Debug resource
+      //System.out.println("Opened database successfully");
 
       String query = "select * from users where username = '" + un + "' limit 1";
-      System.out.println(query);
+      //Debug resource
+      //System.out.println(query);
       ResultSet rs = stmt.executeQuery(query);
+      
       if (rs.next()) {
         String user_id = rs.getString("user_id");
         String username = rs.getString("username");
         String password = rs.getString("password");
         user = new User(user_id, username, password);
       }
+      
       cxn.close();
+      
     } catch (Exception e) {
-      e.printStackTrace();
-      System.err.println(e.getClass().getName()+": "+e.getMessage());
+      //Debug resource
+      //e.printStackTrace();
+      //System.err.println(e.getClass().getName()+": "+e.getMessage());
+      throw new Exception(e.getMessage());
+      
     } finally {
       return user;
     }

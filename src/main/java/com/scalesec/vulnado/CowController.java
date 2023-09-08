0ddb72fb125d108e17ebc5210dd456e2bc3dc3a0
Julio Arruda <julio.arruda@outlook.com>
2023-09-08 16:33:33 -0300

    Correção: Make sure allowing safe and unsafe HTTP methods is safe here.

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -1,3 +1,5 @@
+
+
 package com.scalesec.vulnado;
 
 import org.springframework.web.bind.annotation.*;
@@ -8,7 +10,7 @@ import java.io.Serializable;
 @RestController
 @EnableAutoConfiguration
 public class CowController {
-    @RequestMapping(value = "/cowsay")
+    @RequestMapping(value = "/cowsay", method = RequestMethod.GET)
     String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
         return Cowsay.run(input);
     }

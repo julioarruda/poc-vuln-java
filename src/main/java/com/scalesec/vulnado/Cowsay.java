0822e85bfe498095476f760c12b0721da5d99284
Julio Arruda <julio.arruda@outlook.com>
2023-09-08 16:33:22 -0300

    Correção: Make sure that this user-controlled command argument doesn't lead to unwanted behavior.

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -1,3 +1,6 @@
+-------------------
+
+
 package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
@@ -6,23 +9,25 @@ import java.io.InputStreamReader;
 public class Cowsay {
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
-    processBuilder.command("bash", "-c", cmd);
+    String cmd = "/usr/games/cowsay";
+    processBuilder.command(cmd, input);
 
     StringBuilder output = new StringBuilder();
 
     try {
       Process process = processBuilder.start();
-      BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+      BufferedReader reader =
+          new BufferedReader(new InputStreamReader(process.getInputStream()));
 
       String line;
       while ((line = reader.readLine()) != null) {
         output.append(line + "\n");
       }
+
     } catch (Exception e) {
       e.printStackTrace();
     }
+
     return output.toString();
   }
 }

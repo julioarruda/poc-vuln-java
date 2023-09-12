7a7acc42bda6b215e1337404cbda48a5b444d54a
Julio Arruda <julio.arruda@outlook.com>
2023-09-12 18:37:10 -0300

    Correção: Make sure that this user-controlled command argument doesn't lead to unwanted behavior.

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -1,28 +1,39 @@
+
+
 package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.stream.Stream;
 
 public class Cowsay {
-  public static String run(String input) {
-    ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
-    processBuilder.command("bash", "-c", cmd);
+    public static String run(String input) {
+        String[] bannedTerms = {";", "&&", "|", "&", "$", "`", "\\", "\n"};
+        Stream<String> bannedStream = Stream.of(bannedTerms);
+        boolean isSafe = bannedStream.noneMatch(input::contains);
 
-    StringBuilder output = new StringBuilder();
+        if (!isSafe)
+            throw new IllegalArgumentException("Unsafe input provided");
 
-    try {
-      Process process = processBuilder.start();
-      BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        String cmd = "/usr/games/cowsay '" + input + "'";
+        System.out.println(cmd);
+        processBuilder.command("bash", "-c", cmd);
 
-      String line;
-      while ((line = reader.readLine()) != null) {
-        output.append(line + "\n");
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
+        StringBuilder output = new StringBuilder();
+
+        try {
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return output.toString();
     }
-    return output.toString();
-  }
 }

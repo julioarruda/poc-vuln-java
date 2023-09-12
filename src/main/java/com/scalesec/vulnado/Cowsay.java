8c29274c9826ca4db6db1dda6e7159d67f28f8d7
Julio Arruda <julio.arruda@outlook.com>
2023-09-12 18:44:14 -0300

    Correção: Make sure the "PATH" used to find this command includes only what you intend.

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -1,28 +1,34 @@
+
+
 package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Cowsay {
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
-    processBuilder.command("bash", "-c", cmd);
+    // Sanitize the input to remove special characters
+    String sanitizedInput = input.replaceAll("[^\\w\\s]", "");
+    processBuilder.command("bash", "-c", "/usr/games/cowsay", sanitizedInput);
 
     StringBuilder output = new StringBuilder();
 
     try {
       Process process = processBuilder.start();
-      BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+      BufferedReader reader =
+              new BufferedReader(new InputStreamReader(process.getInputStream()));
 
       String line;
       while ((line = reader.readLine()) != null) {
         output.append(line + "\n");
       }
+
     } catch (Exception e) {
       e.printStackTrace();
     }
+
     return output.toString();
   }
 }

51f60d0803d2ed9b984b4487bc47e8402986e097
Julio Arruda <julio.arruda@outlook.com>
2023-09-13 09:47:33 -0300

    Correção: Make sure allowing safe and unsafe HTTP methods is safe here.

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -1,15 +1,20 @@
+
+
 package com.scalesec.vulnado;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.util.HtmlUtils;
 
 import java.io.Serializable;
 
 @RestController
 @EnableAutoConfiguration
 public class CowController {
-    @RequestMapping(value = "/cowsay")
+    @RequestMapping(value = "/cowsay", method = {RequestMethod.GET, RequestMethod.POST})
     String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
-        return Cowsay.run(input);
+        return Cowsay.run(HtmlUtils.htmlEscape(input));
     }
 }
+
+Observe que a função `HtmlUtils.htmlEscape()` foi usada para escapar o input a fim de reduzir a possibilidade de um ataque XSS, e os métodos GET e POST foram explicitamente definidos como permitidos para a rota "/cowsay".

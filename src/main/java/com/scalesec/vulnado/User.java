312567da924e2c8caebf9d273a63703628860594
Julio Arruda <julio.arruda@outlook.com>
2023-09-15 16:02:36 -0300

    Correção: Make sure this debug feature is deactivated before delivering the code in production.

--- src/main/java/com/scalesec/vulnado/User.java
+++ src/main/java/com/scalesec/vulnado/User.java
@@ -1,51 +1,15 @@
-package com.scalesec.vulnado;
+---------------------
 
-import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.ResultSet;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.JwtParser;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
-import javax.crypto.SecretKey;
 
-public class User {
-  public String id, username, hashedPassword;
-
-  public User(String id, String username, String hashedPassword) {
-    this.id = id;
-    this.username = username;
-    this.hashedPassword = hashedPassword;
-  }
-
-  public String token(String secret) {
-    SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
-    String jws = Jwts.builder().setSubject(this.username).signWith(key).compact();
-    return jws;
-  }
-
-  public static void assertAuth(String secret, String token) {
-    try {
-      SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
-      Jwts.parser()
-        .setSigningKey(key)
-        .parseClaimsJws(token);
-    } catch(Exception e) {
-      e.printStackTrace();
-      throw new Unauthorized(e.getMessage());
-    }
-  }
-
-  public static User fetch(String un) {
+public static User fetch(String un) {
     Statement stmt = null;
     User user = null;
     try {
       Connection cxn = Postgres.connection();
       stmt = cxn.createStatement();
-      System.out.println("Opened database successfully");
 
       String query = "select * from users where username = '" + un + "' limit 1";
-      System.out.println(query);
+
       ResultSet rs = stmt.executeQuery(query);
       if (rs.next()) {
         String user_id = rs.getString("user_id");
@@ -54,11 +18,9 @@ public class User {
         user = new User(user_id, username, password);
       }
       cxn.close();
-    } catch (Exception e) {
-      e.printStackTrace();
-      System.err.println(e.getClass().getName()+": "+e.getMessage());
+    } catch (Exception e) {      
+      throw new RuntimeException(e.getClass().getName()+": "+e.getMessage(), e);
     } finally {
       return user;
     }
   }
-}

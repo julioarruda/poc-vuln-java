063cb1128a1e5bf6b6f952cb0353c1552fe15c22
Julio Arruda <julio.arruda@outlook.com>
2023-09-15 17:46:21 -0300

    Correction: Make sure that this user-controlled command argument doesn't lead to unwanted behavior.

--- src/main/java/com/scalesec/vulnado/LinksController.java
+++ src/main/java/com/scalesec/vulnado/LinksController.java
@@ -7,17 +7,37 @@ import org.springframework.boot.autoconfigure.*;
 import java.util.List;
 import java.io.Serializable;
 import java.io.IOException;
-
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @RestController
 @EnableAutoConfiguration
 public class LinksController {
+  
   @RequestMapping(value = "/links", produces = "application/json")
-  List<String> links(@RequestParam String url) throws IOException{
-    return LinkLister.getLinks(url);
+  List<String> links(@RequestParam String url) throws IOException, MalformedURLException {
+    if(isValidURL(url)){
+      return LinkLister.getLinks(url);
+    }
+    throw new BadRequest("Invalid URL");
   }
+
   @RequestMapping(value = "/links-v2", produces = "application/json")
-  List<String> linksV2(@RequestParam String url) throws BadRequest{
-    return LinkLister.getLinksV2(url);
+  List<String> linksV2(@RequestParam String url) throws BadRequest, MalformedURLException {
+    if(isValidURL(url)){
+      return LinkLister.getLinksV2(url);
+    }
+    throw new BadRequest("Invalid URL");
+  }
+  
+  public static boolean isValidURL(String url) 
+  { 
+    try { 
+      new URL(url).toURI(); 
+      return true; 
+    } 
+    catch (Exception e) { 
+      return false; 
+    } 
   }
 }

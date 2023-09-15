a0b5c7113835e9a9b0d5c157428e1cb6d3225d0c
Julio Arruda <julio.arruda@outlook.com>
2023-09-15 17:54:42 -0300

    Correction: Make sure that this user-controlled command argument doesn't lead to unwanted behavior.

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -2,14 +2,20 @@ package com.scalesec.vulnado;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
-
+import java.util.regex.Pattern;
 import java.io.Serializable;
 
 @RestController
 @EnableAutoConfiguration
 public class CowController {
+
+    private static final Pattern ALLOWED_CHARACTERS = Pattern.compile("[a-zA-Z0-9\\s]*");
+
     @RequestMapping(value = "/cowsay")
     String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
+        if (!ALLOWED_CHARACTERS.matcher(input).matches()) {
+            throw new IllegalArgumentException("Invalid characters in input");
+        }
         return Cowsay.run(input);
     }
 }

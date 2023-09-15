75726cb233e4efc286667b39f140b19251ebdc43
Julio Arruda <julio.arruda@outlook.com>
2023-09-15 17:54:13 -0300

    Correction: Make sure that this user-controlled command argument doesn't lead to unwanted behavior.

--- src/main/java/com/scalesec/vulnado/LoginController.java
+++ src/main/java/com/scalesec/vulnado/LoginController.java
@@ -17,11 +17,16 @@ public class LoginController {
   @CrossOrigin(origins = "*")
   @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
   LoginResponse login(@RequestBody LoginRequest input) {
-    User user = User.fetch(input.username);
-    if (Postgres.md5(input.password).equals(user.hashedPassword)) {
-      return new LoginResponse(user.token(secret));
+    // Validate user input
+    if (input.username != null && input.password != null && input.username.matches("^[a-zA-Z0-9]*$") && input.password.matches("^[a-zA-Z0-9]*$")) {
+      User user = User.fetch(input.username);
+      if (Postgres.md5(input.password).equals(user.hashedPassword)) {
+        return new LoginResponse(user.token(secret));
+      } else {
+        throw new Unauthorized("Access Denied");
+      }
     } else {
-      throw new Unauthorized("Access Denied");
+      throw new Unauthorized("Invalid Input");
     }
   }
 }

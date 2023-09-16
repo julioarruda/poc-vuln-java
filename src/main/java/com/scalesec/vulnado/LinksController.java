04b5b14cba77c8c9732a858f68388a4b70a2ab52
Julio Arruda <julio.arruda@outlook.com>
2023-09-16 15:52:36 -0300

    Correction: Make sure that this user-controlled command argument doesn't lead to unwanted behavior.

--- src/main/java/com/scalesec/vulnado/LinksController.java
+++ src/main/java/com/scalesec/vulnado/LinksController.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.*;
 import java.util.List;
 import java.io.Serializable;
 import java.io.IOException;
+import java.net.URL;
 
 
 @RestController
@@ -14,10 +15,28 @@ import java.io.IOException;
 public class LinksController {
   @RequestMapping(value = "/links", produces = "application/json")
   List<String> links(@RequestParam String url) throws IOException{
-    return LinkLister.getLinks(url);
+    if(validateUrl(url)){
+      return LinkLister.getLinks(url);
+    } else {
+      throw new BadRequest("Invalid URL");
+    }
   }
+
   @RequestMapping(value = "/links-v2", produces = "application/json")
   List<String> linksV2(@RequestParam String url) throws BadRequest{
-    return LinkLister.getLinksV2(url);
+    if(validateUrl(url)){
+      return LinkLister.getLinksV2(url);
+    } else {
+      throw new BadRequest("Invalid URL");
+    }
+  }
+
+  private boolean validateUrl(String url){
+    try{
+      new URL(url).toURI();
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
   }
 }

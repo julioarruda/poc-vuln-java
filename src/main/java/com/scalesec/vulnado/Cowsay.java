96f9801f507a01990713aa60f8a2c5087e677232
Julio Arruda <julio.arruda@outlook.com>
2023-09-19 09:16:29 -0300

    [GFTCodeFix]: Make sure that this user-controlled command argument doesn't lead to unwanted behavior.

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -2,13 +2,14 @@ package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Cowsay {
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
-    processBuilder.command("bash", "-c", cmd);
+    // Remove the command string concatenation with user-provided input
+    // Replace with a list of commands that treats user input as separate arguments
+    processBuilder.command(Arrays.asList("/bin/bash", "-c", "/usr/games/cowsay", input));
 
     StringBuilder output = new StringBuilder();
 

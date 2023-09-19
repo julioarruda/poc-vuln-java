f30b6a4382cd281d0084809b53aea3f30ff413e4
Julio Arruda <julio.arruda@outlook.com>
2023-09-19 09:18:12 -0300

    [GFTCodeFix]: Make sure allowing safe and unsafe HTTP methods is safe here.

--- src/main/java/com/scalesec/vulnado/LinksController.java
+++ src/main/java/com/scalesec/vulnado/LinksController.java
@@ -12,10 +12,14 @@ import java.io.IOException;
 @RestController
 @EnableAutoConfiguration
 public class LinksController {
+
+  @CrossOrigin(methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
   @RequestMapping(value = "/links", produces = "application/json")
   List<String> links(@RequestParam String url) throws IOException{
     return LinkLister.getLinks(url);
   }
+
+  @CrossOrigin(methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
   @RequestMapping(value = "/links-v2", produces = "application/json")
   List<String> linksV2(@RequestParam String url) throws BadRequest{
     return LinkLister.getLinksV2(url);

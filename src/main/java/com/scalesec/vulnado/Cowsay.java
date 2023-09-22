23733e5da3848eab9ab055578ba15ceb80ccf5fb
Julio Arruda <julio.arruda@outlook.com>
2023-09-22 16:19:58 -0300

    [GFTCodeFix]: Make sure the "PATH" used to find this command includes only what you intend.

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -2,27 +2,36 @@ package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.regex.Pattern;
 
 public class Cowsay {
-  public static String run(String input) {
-    ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
-    processBuilder.command("bash", "-c", cmd);
 
-    StringBuilder output = new StringBuilder();
+    private static final Pattern SAFE_PATTERN = Pattern.compile("[a-zA-Z0-9 ]*");
 
-    try {
-      Process process = processBuilder.start();
-      BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+    public static String run(String input) {
+        if (!SAFE_PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException("Invalid input!");
+        }
 
-      String line;
-      while ((line = reader.readLine()) != null) {
-        output.append(line + "\n");
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        String cmd = "/usr/games/cowsay '" + input + "'";
+        System.out.println(cmd);
+        processBuilder.command("bash", "-c", cmd);
+
+        StringBuilder output = new StringBuilder();
+
+        try {
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return output.toString();
     }
-    return output.toString();
-  }
 }

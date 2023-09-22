018e6bf004094ac092da5cf935c1e75b795bb155
Julio Arruda <julio.arruda@outlook.com>
2023-09-22 15:51:17 -0300

    [GFTCodeFix]: Make sure that this user-controlled command argument doesn't lead to unwanted behavior.

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -6,7 +6,8 @@ import java.io.InputStreamReader;
 public class Cowsay {
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
+    String safeInput = input.replaceAll("[^A-Za-z0-9 ]", "");
+    String cmd = "/usr/games/cowsay '" + safeInput + "'";
     System.out.println(cmd);
     processBuilder.command("bash", "-c", cmd);
 

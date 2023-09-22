3776907bac5fc9397c2c2e6d59ef9640845936e5
Julio Arruda <julio.arruda@outlook.com>
2023-09-22 16:10:43 -0300

    [GFTCodeFix]: Make sure this debug feature is deactivated before delivering the code in production.

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -4,25 +4,26 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Cowsay {
-  public static String run(String input) {
-    ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
-    processBuilder.command("bash", "-c", cmd);
+    public static String run(String input) {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        
+        // Evita injeção de comando
+        String cmd = "/usr/games/cowsay";
+        processBuilder.command("bash", "-c", cmd, input);
 
-    StringBuilder output = new StringBuilder();
+        StringBuilder output = new StringBuilder();
 
-    try {
-      Process process = processBuilder.start();
-      BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        try {
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
-      String line;
-      while ((line = reader.readLine()) != null) {
-        output.append(line + "\n");
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return output.toString();
     }
-    return output.toString();
-  }
 }

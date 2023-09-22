20a14120c68baef2d07386e39c491ba247f78c7b
Julio Arruda <julio.arruda@outlook.com>
2023-09-22 16:02:26 -0300

    [GFTCodeFix]: Make sure that enabling CORS is safe here.

--- src/main/java/com/scalesec/vulnado/LoginController.java
+++ src/main/java/com/scalesec/vulnado/LoginController.java
@@ -14,7 +14,6 @@ public class LoginController {
   @Value("${app.secret}")
   private String secret;
 
-  @CrossOrigin(origins = "*")
   @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
   LoginResponse login(@RequestBody LoginRequest input) {
     User user = User.fetch(input.username);

43478d96cf6d94a8888af24b059e2873c7eeec6c
Julio Arruda <julio.arruda@outlook.com>
2023-09-22 16:17:32 -0300

    [GFTCodeFix]: Make sure that enabling CORS is safe here.

--- src/main/java/com/scalesec/vulnado/CommentsController.java
+++ src/main/java/com/scalesec/vulnado/CommentsController.java
@@ -13,20 +13,20 @@ public class CommentsController {
   @Value("${app.secret}")
   private String secret;
 
-  @CrossOrigin(origins = "*")
+  @CrossOrigin(origins = "http://trustedwebsite.com") // Substitua http://trustedwebsite.com pela URL do website confiável
   @RequestMapping(value = "/comments", method = RequestMethod.GET, produces = "application/json")
   List<Comment> comments(@RequestHeader(value="x-auth-token") String token) {
     User.assertAuth(secret, token);
     return Comment.fetch_all();
   }
 
-  @CrossOrigin(origins = "*")
+  @CrossOrigin(origins = "http://trustedwebsite.com") // Substitua http://trustedwebsite.com pela URL do website confiável
   @RequestMapping(value = "/comments", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
   Comment createComment(@RequestHeader(value="x-auth-token") String token, @RequestBody CommentRequest input) {
     return Comment.create(input.username, input.body);
   }
 
-  @CrossOrigin(origins = "*")
+  @CrossOrigin(origins = "http://trustedwebsite.com") // Substitua http://trustedwebsite.com pela URL do website confiável
   @RequestMapping(value = "/comments/{id}", method = RequestMethod.DELETE, produces = "application/json")
   Boolean deleteComment(@RequestHeader(value="x-auth-token") String token, @PathVariable("id") String id) {
     return Comment.delete(id);

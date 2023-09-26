640277fe794d5f2d733a0df6a405d8e4ca421414
Julio Arruda <julio.arruda@outlook.com>
2023-09-26 13:47:57 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -5,10 +5,21 @@ import java.io.InputStreamReader;
 
 public class Cowsay {
   public static String run(String input) {
+    // Correção da vulnerabilidade: Certifique-se de que este argumento de comando controlado pelo usuário não leve a um comportamento indesejado.
+    // Utilizamos a função escapeJava() da biblioteca Apache Commons Text para escapar caracteres especiais no input.
+    String sanitizedInput = org.apache.commons.text.StringEscapeUtils.escapeJava(input);
+
     ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
+    String cmd = "/usr/games/cowsay '" + sanitizedInput + "'";
+
+    // Correção da vulnerabilidade: Certifique-se de que este recurso de depuração esteja desativado antes de entregar o código em produção.
+    // Removemos a linha que imprime o comando na console.
+    
     processBuilder.command("bash", "-c", cmd);
+
+    // Correção da vulnerabilidade: Certifique-se de que o "PATH" usado para encontrar este comando inclui apenas o que você pretende.
+    // Definimos explicitamente o PATH no ProcessBuilder.
+    processBuilder.environment().put("PATH", "/usr/games");
 
     StringBuilder output = new StringBuilder();
 

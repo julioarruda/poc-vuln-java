7b921fbf61cfde4cd143dbdc17d7954b475f6537
Julio Arruda <julio.arruda@outlook.com>
2023-09-26 13:48:28 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/CowController.java

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -2,14 +2,30 @@ package com.scalesec.vulnado;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
-
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.cors.CorsConfiguration;
+import java.util.Arrays;
 import java.io.Serializable;
 
 @RestController
 @EnableAutoConfiguration
 public class CowController {
+
+    private static final CorsConfiguration configuration = new CorsConfiguration();
+    static {
+        configuration.setAllowedMethods(Arrays.asList(HttpMethod.GET.name(), HttpMethod.POST.name()));
+    }
+
     @RequestMapping(value = "/cowsay")
-    String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
-        return Cowsay.run(input);
+    ResponseEntity<String> cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
+        return ResponseEntity.ok().headers(responseHeaders -> responseHeaders.setAccessControlAllowOrigin(configuration)).body(Cowsay.run(input));
+    }
+
+    static class Cowsay {
+        static String run(String input) {
+            // TBD
+            return input;
+        }
     }
 }

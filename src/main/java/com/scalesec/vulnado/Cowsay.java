5d5f2fa42b9f5f9dfecb3a670f09ef9ec4121cc6
Julio Arruda <julio.arruda@outlook.com>
2023-09-29 18:14:30 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -2,27 +2,34 @@ package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.logging.Logger;
 
 public class Cowsay {
-  public static String run(String input) {
-    ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
-    processBuilder.command("bash", "-c", cmd);
+    private static final Logger LOGGER = Logger.getLogger(Cowsay.class.getName());
 
-    StringBuilder output = new StringBuilder();
-
-    try {
-      Process process = processBuilder.start();
-      BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-      String line;
-      while ((line = reader.readLine()) != null) {
-        output.append(line + "\n");
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
+    private Cowsay() {
+        // Private constructor to hide the implicit public one.
     }
-    return output.toString();
-  }
+
+    public static String run(String input) {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        String cmd = "/usr/games/cowsay '" + input.replaceAll("'", "\\'") + "'";
+        LOGGER.info(cmd);
+        processBuilder.command("bash", "-c", cmd);
+
+        StringBuilder output = new StringBuilder();
+
+        try {
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+        } catch (Exception e) {
+            LOGGER.severe(e.getMessage());
+        }
+        return output.toString();
+    }
 }

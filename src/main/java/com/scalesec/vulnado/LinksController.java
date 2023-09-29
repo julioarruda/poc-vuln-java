85776669e3a48518b51c5396148da493f7fe132f
Julio Arruda <julio.arruda@outlook.com>
2023-09-29 08:44:47 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/LinksController.java

--- src/main/java/com/scalesec/vulnado/LinksController.java
+++ src/main/java/com/scalesec/vulnado/LinksController.java
@@ -8,16 +8,17 @@ import java.util.List;
 import java.io.Serializable;
 import java.io.IOException;
 
-
 @RestController
 @EnableAutoConfiguration
 public class LinksController {
-  @RequestMapping(value = "/links", produces = "application/json")
-  List<String> links(@RequestParam String url) throws IOException{
+
+  @RequestMapping(value = "/links", produces = "application/json", method = RequestMethod.GET)
+  public List<String> links(@RequestParam String url) throws IOException{
     return LinkLister.getLinks(url);
   }
-  @RequestMapping(value = "/links-v2", produces = "application/json")
-  List<String> linksV2(@RequestParam String url) throws BadRequest{
+
+  @RequestMapping(value = "/links-v2", produces = "application/json", method = RequestMethod.GET)
+  public List<String> linksV2(@RequestParam String url) throws BadRequest{
     return LinkLister.getLinksV2(url);
   }
 }

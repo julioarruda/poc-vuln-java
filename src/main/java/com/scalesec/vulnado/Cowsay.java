db4263170da3d26328954a16b93fe5e0112aa934
Julio Arruda <julio.arruda@outlook.com>
2023-09-29 08:44:04 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -6,9 +6,16 @@ import java.io.InputStreamReader;
 public class Cowsay {
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
-    processBuilder.command("bash", "-c", cmd);
+    String cmd = "/usr/games/cowsay";
+    String[] safeCmd = {cmd, input};
+
+    // Deactivate debug feature
+    // System.out.println(cmd);
+
+    processBuilder.command(safeCmd);
+
+    // Avoid unwanted PATH behavior
+    processBuilder.environment().put("PATH", "/usr/games");
 
     StringBuilder output = new StringBuilder();
 

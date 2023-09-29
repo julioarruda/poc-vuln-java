8b1ba607a82e9da5e636dc8f3e830506c2da674e
Julio Arruda <julio.arruda@outlook.com>
2023-09-29 18:14:59 -0300

    [GFTCodeFix]: Update on  src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java

--- src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java
+++ src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java
@@ -2,16 +2,29 @@ package com.scalesec.vulnado;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class VulnadoApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	public void contextLoads() {
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/", String.class);
+		assertThat(response.getStatusCodeValue()).isEqualTo(200);
 	}
-
 }
-

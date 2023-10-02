08586700faab122996860f6228c2b5c0abf18108
Julio Arruda <julio.arruda@outlook.com>
2023-10-02 17:20:43 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -2,19 +2,26 @@ package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.nio.file.Paths;
 
 public class Cowsay {
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
-    processBuilder.command("bash", "-c", cmd);
+    String cmd = "/usr/games/cowsay";
+    String sanitizedInput = sanitizeInput(input);
+    
+    // Remove the debug print statement
+    // System.out.println(cmd);
+
+    processBuilder.command("bash", "-c", Paths.get(cmd, sanitizedInput).toString());
+    processBuilder.environment().put("PATH", "/usr/games");
 
     StringBuilder output = new StringBuilder();
 
     try {
       Process process = processBuilder.start();
-      BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+      BufferedReader reader = 
+             new BufferedReader(new InputStreamReader(process.getInputStream()));
 
       String line;
       while ((line = reader.readLine()) != null) {
@@ -24,5 +31,10 @@ public class Cowsay {
       e.printStackTrace();
     }
     return output.toString();
+  }
+
+  // Method to sanitize the user input
+  private static String sanitizeInput(String input) {
+    return input.replaceAll("[^a-zA-Z0-9]", "");
   }
 }

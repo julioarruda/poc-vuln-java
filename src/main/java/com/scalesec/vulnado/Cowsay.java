52fd51569ba39e546fe2c04322d1a2182449faef
Julio Arruda <julio.arruda@outlook.com>
2023-10-03 15:00:04 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -6,9 +6,14 @@ import java.io.InputStreamReader;
 public class Cowsay {
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
+    // Make sure that this user-controlled command argument doesn't lead to unwanted behavior.
+    String sanitizedInput = sanitizeInput(input);
+    String cmd = "/usr/games/cowsay '" + sanitizedInput + "'";
+    // Make sure this debug feature is deactivated before delivering the code in production.
+    // System.out.println(cmd);
     processBuilder.command("bash", "-c", cmd);
+    // Make sure the "PATH" used to find this command includes only what you intend.
+    processBuilder.environment().put("PATH", "/usr/games/");
 
     StringBuilder output = new StringBuilder();
 
@@ -24,5 +29,9 @@ public class Cowsay {
       e.printStackTrace();
     }
     return output.toString();
+  }
+
+  private static String sanitizeInput(String input) {
+    return input.replaceAll("[^\\w\\s]","");
   }
 }

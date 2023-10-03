d2ca5547bf02eead1952aa82c80cf0d9101ad321
Julio Arruda <julio.arruda@outlook.com>
2023-10-03 12:53:49 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -6,8 +6,16 @@ import java.io.InputStreamReader;
 public class Cowsay {
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
+    // Correção da Vulnerabilidade: Garantindo que o comando controlado pelo usuário não leve a comportamentos indesejados.
+    // Para isso, vamos sanitizar a entrada do usuário para evitar a execução de comandos indesejados
+    String sanitizedInput = input.replaceAll("[^a-zA-Z0-9 ]", "");
+    
+    String cmd = "/usr/games/cowsay '" + sanitizedInput + "'";
+    
+    // Correção da Vulnerabilidade: Garantir que este recurso de debug esteja desativado antes de entregar o código em produção.
+    // Vamos comentar a linha de impressão do comando
+    // System.out.println(cmd);
+    
     processBuilder.command("bash", "-c", cmd);
 
     StringBuilder output = new StringBuilder();

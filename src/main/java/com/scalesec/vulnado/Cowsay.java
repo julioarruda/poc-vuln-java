1c8ffcbc2d56e522a59d5f3507d90d8bf66a60e5
Julio Arruda <julio.arruda@outlook.com>
2023-10-03 16:30:10 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -2,12 +2,26 @@ package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.nio.file.Paths;
 
 public class Cowsay {
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
+    String cmd = "/usr/games/cowsay";
+    // Validate input to ensure it doesn't contain unwanted command
+    if (input != null && !input.contains(";") && !input.contains("|") && !input.contains("&")) {
+      cmd += " '" + input + "'";
+    } else {
+      throw new IllegalArgumentException("Invalid input!");
+    }
+
+    // Remove debug print statement
+    // System.out.println(cmd);
+
+    // Make sure the "PATH" used to find this command includes only what you intend.
+    String path = Paths.get("/usr/games").toString();
+    processBuilder.environment().put("PATH", path);
+
     processBuilder.command("bash", "-c", cmd);
 
     StringBuilder output = new StringBuilder();

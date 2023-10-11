870dc992d4969b10ff322fe1a42da871aa7d1463
Julio Arruda <julio.arruda@outlook.com>
2023-10-11 15:38:16 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -2,13 +2,23 @@ package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.logging.Logger;
 
 public class Cowsay {
+  private static final Logger LOGGER = Logger.getLogger(Cowsay.class.getName());
+
+  private Cowsay() {
+    // Private constructor to hide the implicit public one.
+  }
+
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
+    String cmd = "/usr/games/cowsay '" + input.replace("'", "\\'") + "'"; // Make sure that this user-controlled command argument doesn't lead to unwanted behavior.
+
+    LOGGER.info(cmd); // Replace this use of System.out or System.err by a logger.
+
     processBuilder.command("bash", "-c", cmd);
+    processBuilder.environment().put("PATH", "/usr/games"); // Make sure the "PATH" used to find this command includes only what you intend.
 
     StringBuilder output = new StringBuilder();
 
@@ -21,7 +31,7 @@ public class Cowsay {
         output.append(line + "\n");
       }
     } catch (Exception e) {
-      e.printStackTrace();
+      LOGGER.severe(e.getMessage());
     }
     return output.toString();
   }

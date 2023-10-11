e591c8ae9969b42a0c859ef39540c5f822755342
Julio Arruda <julio.arruda@outlook.com>
2023-10-11 09:36:03 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -2,19 +2,25 @@ package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Cowsay {
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
-    processBuilder.command("bash", "-c", cmd);
+    // Make sure that this user-controlled command argument doesn't lead to unwanted behavior
+    String sanitizedInput = sanitizeInput(input);
+    List<String> commands = new ArrayList<String>();
+    commands.add("/usr/games/cowsay");
+    commands.add(sanitizedInput);
 
+    processBuilder.command(commands);
     StringBuilder output = new StringBuilder();
 
     try {
       Process process = processBuilder.start();
-      BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+      BufferedReader reader = 
+              new BufferedReader(new InputStreamReader(process.getInputStream()));
 
       String line;
       while ((line = reader.readLine()) != null) {
@@ -23,6 +29,13 @@ public class Cowsay {
     } catch (Exception e) {
       e.printStackTrace();
     }
+    // Make sure this debug feature is deactivated before delivering the code in production
+    // System.out.println(cmd);
     return output.toString();
+  }
+
+  // sanitizeInput method to prevent unwanted behavior from user-controlled command argument
+  private static String sanitizeInput(String input) {
+    return input.replaceAll("[^a-zA-Z0-9 ]", "");
   }
 }

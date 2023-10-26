0b1e7d7bb7fd162edd9e1bb7bbc2020952442b93
Julio Arruda <julio.arruda@outlook.com>
2023-10-26 09:09:26 -0300

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/User.java

--- src/main/java/com/scalesec/vulnado/User.java
+++ src/main/java/com/scalesec/vulnado/User.java
@@ -1,27 +1,49 @@
 package com.scalesec.vulnado;
 
 import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.logging.Logger;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.JwtParser;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import javax.crypto.SecretKey;
 
 public class User {
-  public String id, username, hashedPassword;
+  private static final Logger log = Logger.getLogger(User.class.getName());
+
+  private String id;
+  private String username;
+  private String hashedPassword;
 
   public User(String id, String username, String hashedPassword) {
     this.id = id;
+    setUsername(username);
+    setHashedPassword(hashedPassword);
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  private void setUsername(String username) {
     this.username = username;
+  }
+
+  public String getHashedPassword() {
+    return hashedPassword;
+  }
+
+  private void setHashedPassword(String hashedPassword) {
     this.hashedPassword = hashedPassword;
   }
 
   public String token(String secret) {
     SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
-    String jws = Jwts.builder().setSubject(this.username).signWith(key).compact();
-    return jws;
+    return Jwts.builder().setSubject(this.username).signWith(key).compact();
   }
 
   public static void assertAuth(String secret, String token) {
@@ -31,34 +53,30 @@ public class User {
         .setSigningKey(key)
         .parseClaimsJws(token);
     } catch(Exception e) {
-      e.printStackTrace();
+      log.severe(e.getMessage());
       throw new Unauthorized(e.getMessage());
     }
   }
 
   public static User fetch(String un) {
-    Statement stmt = null;
     User user = null;
-    try {
-      Connection cxn = Postgres.connection();
-      stmt = cxn.createStatement();
-      System.out.println("Opened database successfully");
+    try (Connection cxn = Postgres.connection();
+         PreparedStatement stmt = cxn.prepareStatement("select * from users where username = ? limit 1")) {
 
-      String query = "select * from users where username = '" + un + "' limit 1";
-      System.out.println(query);
-      ResultSet rs = stmt.executeQuery(query);
-      if (rs.next()) {
-        String user_id = rs.getString("user_id");
-        String username = rs.getString("username");
-        String password = rs.getString("password");
-        user = new User(user_id, username, password);
+      stmt.setString(1, un);
+      log.info("Opened database successfully");
+
+      try (ResultSet rs = stmt.executeQuery()) {
+        if (rs.next()) {
+          String userId = rs.getString("user_id");
+          String userName = rs.getString("username");
+          String password = rs.getString("password");
+          user = new User(userId, userName, password);
+        }
       }
-      cxn.close();
     } catch (Exception e) {
-      e.printStackTrace();
-      System.err.println(e.getClass().getName()+": "+e.getMessage());
-    } finally {
-      return user;
+      log.severe(e.getClass().getName()+": "+e.getMessage());
     }
+    return user;
   }
 }

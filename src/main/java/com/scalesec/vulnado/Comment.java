fa62bcb60dbd3b4710f1197cf3d70945eed0f66f
Julio Arruda <julio.arruda@outlook.com>
2023-11-10 13:20:16 -0800

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/Comment.java

--- src/main/java/com/scalesec/vulnado/Comment.java
+++ src/main/java/com/scalesec/vulnado/Comment.java
@@ -1,22 +1,29 @@
 package com.scalesec.vulnado;
 
-import org.apache.catalina.Server;
 import java.sql.*;
 import java.util.Date;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.UUID;
+import java.util.logging.Logger; // GFT AI Impact Bot - Replaced System.out with Logger
 
 public class Comment {
-  public String id, username, body;
-  public Timestamp created_on;
+  private static final Logger LOGGER = Logger.getLogger(Comment.class.getName()); // GFT AI Impact Bot - Created Logger
 
-  public Comment(String id, String username, String body, Timestamp created_on) {
+  private String id, username, body; // GFT AI Impact Bot - Made id, username, and body non-public
+  private Timestamp createdOn; // GFT AI Impact Bot - Made created_on non-public and renamed to match regex
+
+  public Comment(String id, String username, String body, Timestamp createdOn) {
     this.id = id;
     this.username = username;
     this.body = body;
-    this.created_on = created_on;
+    this.createdOn = createdOn; // GFT AI Impact Bot - Rename to match the regular expression
   }
+
+  public String getId() { return this.id; } // GFT AI Impact Bot - Added accessor for id
+  public String getUsername() { return this.username; } // GFT AI Impact Bot - Added accessor for username
+  public String getBody() { return this.body; } // GFT AI Impact Bot - Added accessor for body
+  public Timestamp getCreatedOn() { return this.createdOn; } // GFT AI Impact Bot - Added accessor for created_on
 
   public static Comment create(String username, String body){
     long time = new Date().getTime();
@@ -33,12 +40,9 @@ public class Comment {
     }
   }
 
-  public static List<Comment> fetch_all() {
-    Statement stmt = null;
-    List<Comment> comments = new ArrayList();
-    try {
-      Connection cxn = Postgres.connection();
-      stmt = cxn.createStatement();
+  public static List<Comment> fetchAll() { // GFT AI Impact Bot - Renamed method to match regex
+    List<Comment> comments = new ArrayList<>();
+    try (Connection cxn = Postgres.connection(); Statement stmt = cxn.createStatement()) { // GFT AI Impact Bot - Used try-with-resources
 
       String query = "select * from comments;";
       ResultSet rs = stmt.executeQuery(query);
@@ -46,41 +50,34 @@ public class Comment {
         String id = rs.getString("id");
         String username = rs.getString("username");
         String body = rs.getString("body");
-        Timestamp created_on = rs.getTimestamp("created_on");
-        Comment c = new Comment(id, username, body, created_on);
+        Timestamp createdOn = rs.getTimestamp("created_on"); // GFT AI Impact Bot - Renamed to match the regular expression
+        Comment c = new Comment(id, username, body, createdOn);
         comments.add(c);
       }
-      cxn.close();
     } catch (Exception e) {
-      e.printStackTrace();
-      System.err.println(e.getClass().getName()+": "+e.getMessage());
-    } finally {
-      return comments;
+      LOGGER.severe(e.getMessage()); // GFT AI Impact Bot - Replaced System.err with Logger
     }
+    return comments; // GFT AI Impact Bot - Moved return statement out of finally block
   }
 
-  public static Boolean delete(String id) {
-    try {
-      String sql = "DELETE FROM comments where id = ?";
-      Connection con = Postgres.connection();
-      PreparedStatement pStatement = con.prepareStatement(sql);
+  public static boolean delete(String id) { // GFT AI Impact Bot - Used primitive boolean
+    try (Connection con = Postgres.connection(); PreparedStatement pStatement = con.prepareStatement("DELETE FROM comments where id = ?")) { // GFT AI Impact Bot - Used try-with-resources
       pStatement.setString(1, id);
       return 1 == pStatement.executeUpdate();
     } catch(Exception e) {
-      e.printStackTrace();
-    } finally {
-      return false;
+      LOGGER.severe(e.getMessage()); // GFT AI Impact Bot - Replaced e.printStackTrace() with Logger
     }
+    return false; // GFT AI Impact Bot - Moved return statement out of finally block
   }
 
-  private Boolean commit() throws SQLException {
+  private boolean commit() throws SQLException { // GFT AI Impact Bot - Used primitive boolean
     String sql = "INSERT INTO comments (id, username, body, created_on) VALUES (?,?,?,?)";
-    Connection con = Postgres.connection();
-    PreparedStatement pStatement = con.prepareStatement(sql);
-    pStatement.setString(1, this.id);
-    pStatement.setString(2, this.username);
-    pStatement.setString(3, this.body);
-    pStatement.setTimestamp(4, this.created_on);
-    return 1 == pStatement.executeUpdate();
+    try (Connection con = Postgres.connection(); PreparedStatement pStatement = con.prepareStatement(sql)) { // GFT AI Impact Bot - Used try-with-resources
+      pStatement.setString(1, this.id);
+      pStatement.setString(2, this.username);
+      pStatement.setString(3, this.body);
+      pStatement.setTimestamp(4, this.createdOn); // GFT AI Impact Bot - Renamed to match the regular expression
+      return 1 == pStatement.executeUpdate();
+    }
   }
 }

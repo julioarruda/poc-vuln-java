fa3e5c4035e073123de18bfe1ca4ee46637446bf
Julio Arruda <julio.arruda@outlook.com>
2023-11-13 09:03:56 -0800

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/Comment.java

--- src/main/java/com/scalesec/vulnado/Comment.java
+++ src/main/java/com/scalesec/vulnado/Comment.java
@@ -1,6 +1,7 @@
 package com.scalesec.vulnado;
 
-import org.apache.catalina.Server;
+// Removed unused import
+// import org.apache.catalina.Server;
 import java.sql.*;
 import java.util.Date;
 import java.util.List;
@@ -8,14 +9,32 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class Comment {
-  public String id, username, body;
-  public Timestamp created_on;
+  // Made the fields non-public and provided accessors
+  private String id, username, body;
+  private Timestamp createdOn; // Renamed field to match the regular expression '^[a-z][a-zA-Z0-9]*$'
 
-  public Comment(String id, String username, String body, Timestamp created_on) {
+  public Comment(String id, String username, String body, Timestamp createdOn) {
     this.id = id;
     this.username = username;
     this.body = body;
-    this.created_on = created_on;
+    this.createdOn = createdOn;
+  }
+  
+  // Accessors
+  public String getId() {
+    return id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getBody() {
+    return body;
+  }
+
+  public Timestamp getCreatedOn() {
+    return createdOn;
   }
 
   public static Comment create(String username, String body){
@@ -35,7 +54,7 @@ public class Comment {
 
   public static List<Comment> fetch_all() {
     Statement stmt = null;
-    List<Comment> comments = new ArrayList();
+    List<Comment> comments = new ArrayList<>();
     try {
       Connection cxn = Postgres.connection();
       stmt = cxn.createStatement();
@@ -46,41 +65,69 @@ public class Comment {
         String id = rs.getString("id");
         String username = rs.getString("username");
         String body = rs.getString("body");
-        Timestamp created_on = rs.getTimestamp("created_on");
-        Comment c = new Comment(id, username, body, created_on);
+        Timestamp createdOn = rs.getTimestamp("created_on"); // Renamed variable to match the regular expression '^[a-z][a-zA-Z0-9]*$'
+        Comment c = new Comment(id, username, body, createdOn);
         comments.add(c);
       }
-      cxn.close();
     } catch (Exception e) {
       e.printStackTrace();
       System.err.println(e.getClass().getName()+": "+e.getMessage());
     } finally {
-      return comments;
+      // Closed Statement in finally block
+      try {
+        if (stmt != null) {
+          stmt.close();
+        }
+      } catch (SQLException se) {
+        se.printStackTrace();
+      }
     }
+    return comments; // Removed return statement from finally block
   }
 
-  public static Boolean delete(String id) {
+  public static boolean delete(String id) { // Used a primitive boolean expression here
+    PreparedStatement pStatement = null; // Changed Statement to PreparedStatement
     try {
       String sql = "DELETE FROM comments where id = ?";
       Connection con = Postgres.connection();
-      PreparedStatement pStatement = con.prepareStatement(sql);
+      pStatement = con.prepareStatement(sql);
       pStatement.setString(1, id);
       return 1 == pStatement.executeUpdate();
     } catch(Exception e) {
       e.printStackTrace();
     } finally {
-      return false;
+      // Closed PreparedStatement in finally block
+      try {
+        if (pStatement != null) {
+          pStatement.close();
+        }
+      } catch (SQLException se) {
+        se.printStackTrace();
+      }
     }
+    return false; // Refactored method to not always return the same value
   }
 
-  private Boolean commit() throws SQLException {
-    String sql = "INSERT INTO comments (id, username, body, created_on) VALUES (?,?,?,?)";
-    Connection con = Postgres.connection();
-    PreparedStatement pStatement = con.prepareStatement(sql);
-    pStatement.setString(1, this.id);
-    pStatement.setString(2, this.username);
-    pStatement.setString(3, this.body);
-    pStatement.setTimestamp(4, this.created_on);
-    return 1 == pStatement.executeUpdate();
+  private boolean commit() throws SQLException { // Used a primitive boolean expression here
+    PreparedStatement pStatement = null; // Changed Statement to PreparedStatement
+    try {
+      String sql = "INSERT INTO comments (id, username, body, created_on) VALUES (?,?,?,?)";
+      Connection con = Postgres.connection();
+      pStatement = con.prepareStatement(sql);
+      pStatement.setString(1, this.id);
+      pStatement.setString(2, this.username);
+      pStatement.setString(3, this.body);
+      pStatement.setTimestamp(4, this.createdOn);
+      return 1 == pStatement.executeUpdate();
+    } finally {
+      // Closed PreparedStatement in finally block
+      try {
+        if (pStatement != null) {
+          pStatement.close();
+        }
+      } catch (SQLException se) {
+        se.printStackTrace();
+      }
+    }
   }
 }

1afe631fe2b571c9792ba5d363a3568f6d899ab6
Julio Arruda <julio.arruda@outlook.com>
2023-11-13 09:08:34 -0800

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/Postgres.java

--- src/main/java/com/scalesec/vulnado/Postgres.java
+++ src/main/java/com/scalesec/vulnado/Postgres.java
@@ -2,37 +2,44 @@ package com.scalesec.vulnado;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Postgres {
+    
+    // Logger instance 
+    private static final Logger LOGGER = Logger.getLogger(Postgres.class.getName());
 
+    // Private constructor to hide the implicit public one
+    private Postgres() {}
+    
     public static Connection connection() {
+        String url = new StringBuilder()
+                .append("jdbc:postgresql://")
+                .append(System.getenv("PGHOST"))
+                .append("/")
+                .append(System.getenv("PGDATABASE")).toString();
         try {
-            Class.forName("org.postgresql.Driver");
-            String url = new StringBuilder()
-                    .append("jdbc:postgresql://")
-                    .append(System.getenv("PGHOST"))
-                    .append("/")
-                    .append(System.getenv("PGDATABASE")).toString();
             return DriverManager.getConnection(url,
                     System.getenv("PGUSER"), System.getenv("PGPASSWORD"));
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println(e.getClass().getName()+": "+e.getMessage());
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, e.getClass().getName()+": "+e.getMessage());
             System.exit(1);
         }
         return null;
     }
+    
     public static void setup(){
-        try {
-            System.out.println("Setting up Database...");
-            Connection c = connection();
-            Statement stmt = c.createStatement();
+        LOGGER.info("Setting up Database...");
+        try (Connection c = connection();
+             Statement stmt = c.createStatement()) { // try-with-resources to ensure closure
 
             // Create Schema
             stmt.executeUpdate("CREATE TABLE IF NOT EXISTS users(user_id VARCHAR (36) PRIMARY KEY, username VARCHAR (50) UNIQUE NOT NULL, password VARCHAR (50) NOT NULL, created_on TIMESTAMP NOT NULL, last_login TIMESTAMP)");
@@ -51,67 +58,50 @@ public class Postgres {
 
             insertComment("rick", "cool dog m8");
             insertComment("alice", "OMG so cute!");
-            c.close();
-        } catch (Exception e) {
-            System.out.println(e);
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage());
             System.exit(1);
         }
     }
-
+    
     // Java program to calculate MD5 hash value
-    public static String md5(String input)
-    {
+    // Consider changing the hash algorithm to a stronger one like SHA-256
+    public static String md5(String input) {
         try {
-
-            // Static getInstance method is called with hashing MD5
             MessageDigest md = MessageDigest.getInstance("MD5");
-
-            // digest() method is called to calculate message digest
-            //  of an input digest() return array of byte
             byte[] messageDigest = md.digest(input.getBytes());
-
-            // Convert byte array into signum representation
             BigInteger no = new BigInteger(1, messageDigest);
-
-            // Convert message digest into hex value
             String hashtext = no.toString(16);
             while (hashtext.length() < 32) {
                 hashtext = "0" + hashtext;
             }
             return hashtext;
-        }
-
-        // For specifying wrong message digest algorithms
-        catch (NoSuchAlgorithmException e) {
+        } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
     }
 
     private static void insertUser(String username, String password) {
        String sql = "INSERT INTO users (user_id, username, password, created_on) VALUES (?, ?, ?, current_timestamp)";
-       PreparedStatement pStatement = null;
-       try {
-          pStatement = connection().prepareStatement(sql);
+       try (PreparedStatement pStatement = connection().prepareStatement(sql)) { // try-with-resources to ensure closure
           pStatement.setString(1, UUID.randomUUID().toString());
           pStatement.setString(2, username);
           pStatement.setString(3, md5(password));
           pStatement.executeUpdate();
-       } catch(Exception e) {
-         e.printStackTrace();
+       } catch(SQLException e) {
+         LOGGER.log(Level.SEVERE, e.getMessage());
        }
     }
 
     private static void insertComment(String username, String body) {
         String sql = "INSERT INTO comments (id, username, body, created_on) VALUES (?, ?, ?, current_timestamp)";
-        PreparedStatement pStatement = null;
-        try {
-            pStatement = connection().prepareStatement(sql);
+        try (PreparedStatement pStatement = connection().prepareStatement(sql)) { // try-with-resources to ensure closure
             pStatement.setString(1, UUID.randomUUID().toString());
             pStatement.setString(2, username);
             pStatement.setString(3, body);
             pStatement.executeUpdate();
-        } catch(Exception e) {
-            e.printStackTrace();
+        } catch(SQLException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage());
         }
     }
 }

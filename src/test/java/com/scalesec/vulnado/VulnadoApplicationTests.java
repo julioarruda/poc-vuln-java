77b21ef2737a7f0a5401054b249ebdbd3b51b41e
Julio Arruda <julio.arruda@outlook.com>
2023-11-14 10:12:04 -0800

    [GFTCodeFix]: Update on  src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java

--- src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java
+++ src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java
@@ -2,16 +2,21 @@ package com.scalesec.vulnado;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class VulnadoApplicationTests {
 
+    @Autowired
+    private VulnadoApplication vulnadoApplication; // Incluido por GFT AI Impact Bot
+
 	@Test
 	public void contextLoads() {
+        assertThat(vulnadoApplication).isNotNull(); // Incluido por GFT AI Impact Bot
 	}
 
 }
-

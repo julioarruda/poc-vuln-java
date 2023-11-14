82c03f5320ce5f5a36f19d803270c08836fd1ee5
Julio Arruda <julio.arruda@outlook.com>
2023-11-14 10:08:24 -0800

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/LinkLister.java

--- src/main/java/com/scalesec/vulnado/LinkLister.java
+++ src/main/java/com/scalesec/vulnado/LinkLister.java
@@ -8,11 +8,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.io.IOException;
 import java.net.*;
-
+import java.util.logging.Logger; // Incluido por GFT AI Impact Bot
 
 public class LinkLister {
+  private static final Logger LOGGER = Logger.getLogger(LinkLister.class.getName()); // Incluido por GFT AI Impact Bot
+
+  private LinkLister() {} // Incluido por GFT AI Impact Bot
+
   public static List<String> getLinks(String url) throws IOException {
-    List<String> result = new ArrayList<String>();
+    List<String> result = new ArrayList<>(); // Alterado por GFT AI Impact Bot
     Document doc = Jsoup.connect(url).get();
     Elements links = doc.select("a");
     for (Element link : links) {
@@ -25,7 +29,7 @@ public class LinkLister {
     try {
       URL aUrl= new URL(url);
       String host = aUrl.getHost();
-      System.out.println(host);
+      LOGGER.info(host); // Alterado por GFT AI Impact Bot
       if (host.startsWith("172.") || host.startsWith("192.168") || host.startsWith("10.")){
         throw new BadRequest("Use of Private IP");
       } else {

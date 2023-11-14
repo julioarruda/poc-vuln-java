4ea72413075d31ff912ebaa5e78cd97d7baac4aa
Julio Arruda <julio.arruda@outlook.com>
2023-11-14 10:11:41 -0800

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -2,12 +2,19 @@ package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.logging.Logger; // Incluido por GFT AI Impact Bot
 
 public class Cowsay {
+  private static final Logger logger = Logger.getLogger(Cowsay.class.getName()); // Incluido por GFT AI Impact Bot
+
+  private Cowsay() { // Incluido por GFT AI Impact Bot
+    // Private constructor to hide the implicit public one
+  }
+
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
     String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
+    logger.info(cmd); // Alterado por GFT AI Impact Bot
     processBuilder.command("bash", "-c", cmd);
 
     StringBuilder output = new StringBuilder();
@@ -21,7 +28,7 @@ public class Cowsay {
         output.append(line + "\n");
       }
     } catch (Exception e) {
-      e.printStackTrace();
+      logger.severe(e.getMessage()); // Alterado por GFT AI Impact Bot
     }
     return output.toString();
   }

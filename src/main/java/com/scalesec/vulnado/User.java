2c03161fc638a2047123048b89ccf8afc6defaf5
Julio Arruda <julio.arruda@outlook.com>
2023-11-14 10:01:17 -0800

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/User.java

--- src/main/java/com/scalesec/vulnado/User.java
+++ src/main/java/com/scalesec/vulnado/User.java
@@ -1,16 +1,16 @@
 package com.scalesec.vulnado;
 
 import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.logging.Logger;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.JwtParser;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import javax.crypto.SecretKey;
 
 public class User {
-  public String id, username, hashedPassword;
+  private static final Logger logger = Logger.getLogger(User.class.getName()); // CVE Alterado por GFT AI Impact Bot
+  private String id, username, hashedPassword; // CVE Alterado por GFT AI Impact Bot
 
   public User(String id, String username, String hashedPassword) {
     this.id = id;
@@ -18,10 +18,21 @@ public class User {
     this.hashedPassword = hashedPassword;
   }
 
+  public String getId() { // CVE Alterado por GFT AI Impact Bot
+    return id;
+  }
+
+  public String getUsername() { // CVE Alterado por GFT AI Impact Bot
+    return username;
+  }
+
+  public String getHashedPassword() { // CVE Alterado por GFT AI Impact Bot
+    return hashedPassword;
+  }
+
   public String token(String secret) {
     SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
-    String jws = Jwts.builder().setSubject(this.username).signWith(key).compact();
-    return jws;
+    return Jwts.builder().setSubject(this.username).signWith(key).compact(); // CVE Alterado por GFT AI Impact Bot
   }
 
   public static void assertAuth(String secret, String token) {
@@ -31,34 +42,29 @@ public class User {
         .setSigningKey(key)
         .parseClaimsJws(token);
     } catch(Exception e) {
-      e.printStackTrace();
+      logger.severe(e.getMessage()); // CVE Alterado por GFT AI Impact Bot
       throw new Unauthorized(e.getMessage());
     }
   }
 
   public static User fetch(String un) {
-    Statement stmt = null;
+    PreparedStatement stmt = null; // CVE Alterado por GFT AI Impact Bot
     User user = null;
-    try {
-      Connection cxn = Postgres.connection();
-      stmt = cxn.createStatement();
-      System.out.println("Opened database successfully");
+    try (Connection cxn = Postgres.connection()) { // CVE Alterado por GFT AI Impact Bot
+      stmt = cxn.prepareStatement("select * from users where username = ? limit 1"); // CVE Alterado por GFT AI Impact Bot
+      stmt.setString(1, un); // CVE Alterado por GFT AI Impact Bot
+      logger.info("Opened database successfully"); // CVE Alterado por GFT AI Impact Bot
 
-      String query = "select * from users where username = '" + un + "' limit 1";
-      System.out.println(query);
-      ResultSet rs = stmt.executeQuery(query);
+      ResultSet rs = stmt.executeQuery();
       if (rs.next()) {
-        String user_id = rs.getString("user_id");
+        String userId = rs.getString("user_id"); // CVE Alterado por GFT AI Impact Bot
         String username = rs.getString("username");
         String password = rs.getString("password");
-        user = new User(user_id, username, password);
+        user = new User(userId, username, password);
       }
-      cxn.close();
     } catch (Exception e) {
-      e.printStackTrace();
-      System.err.println(e.getClass().getName()+": "+e.getMessage());
-    } finally {
-      return user;
+      logger.severe(e.getClass().getName()+": "+e.getMessage()); // CVE Alterado por GFT AI Impact Bot
     }
+    return user; // CVE Alterado por GFT AI Impact Bot
   }
 }

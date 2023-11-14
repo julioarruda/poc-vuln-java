a9524f9784ef021dafa544bedcd6208261eec450
Julio Arruda <julio.arruda@outlook.com>
2023-11-14 10:07:34 -0800

    [GFTCodeFix]: Update on  src/main/java/com/scalesec/vulnado/CowController.java

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -3,12 +3,12 @@ package com.scalesec.vulnado;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
 
-import java.io.Serializable;
+// Removed unused import 'java.io.Serializable'; // Alterado por GFT AI Impact Bot
 
 @RestController
 @EnableAutoConfiguration
 public class CowController {
-    @RequestMapping(value = "/cowsay")
+    @RequestMapping(value = "/cowsay", method = RequestMethod.GET) // Make sure allowing safe and unsafe HTTP methods is safe here. // Alterado por GFT AI Impact Bot
     String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
         return Cowsay.run(input);
     }
